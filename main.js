37b4d18657058cc706c4c4420663f7d3607257c8
KirianaBrown <kjb0369@arastudent.ac.nz>
2019-04-01 22:44:21 +1300

    Add style changes on the error alert message
    Comments

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
   1. get value from input field and if null then error else 
   2. generate random number using math methods
   3. random number = the src image update to replace the magic 8 ball
-  4. reset button to revert back and ask a new question
+  4. reset button to revert back and ask a new question and hide the error message
   5. add shake annimation
 
 
@@ -30,6 +30,19 @@ document.querySelector('.btn-shake').addEventListener('click', function() {
 
 
 document.querySelector('.btn-shake').addEventListener('click', function() {
+
+    // 1. Get the input value 
     let input = document.querySelector('#inputField').value;
-    console.log(input)
+    // console.log(input)
+
+    // 2.  If empty display error else update the src of the image with the random number generated
+
+    if (input === '') {
+        document.querySelector('.no-input-message').style.display = 'block';
+    } else {
+        document.querySelector('.no-input-message').style.display = 'none';
+        let randomNumber = Math.floor(Math.random() * 5 + 1);
+        let magicBall = document.querySelector('.game-ball');
+        magicBall.src = './src/assets/ball-' + randomNumber + '.png';
+    }
 })
