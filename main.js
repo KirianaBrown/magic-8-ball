cb4b89474fee303f33cc0ec7fb7fed71436ad73b
KirianaBrown <kjb0369@arastudent.ac.nz>
2019-04-02 14:51:02 +1300

    Add image after a timeout to align with shake

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ document.querySelector('.btn-shake').addEventListener('click', function() {
 
 
         setTimeout(function() {
-            // magicBall.src = './src/assets/ball-' + randomNumber + '.png';
+            magicBall.src = './src/assets/ball-' + randomNumber + '.png';
             console.log('update the content in the ball')
         }, 1000);
 
